a30637960481dad64820c0f8f180206bde86433d
Anacris Dias de França Oliveira <anacrisdfranca@gmail.com>
2022-12-21 17:49:03 -0300

    alteracao

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,13 @@
-# criar a função operação valida
-# mostar se operação e valida 
-
 
 print("calculadora")
 
+ 	 	
 def somar(a,b):
 	print("Soma")
 	resultado = int(a) + int(b)
 	return resultado
 	
+		
 def menos(a,b):
 	print("Menos")
 	resultado = int(a) - int(b)
@@ -24,15 +23,33 @@ def divisao(a,b):
 	resultado = int(a) / int(b)
 	return resultado
 
+def operacaoValida(operacao):
+	if operacao == "+":
+		print ("somar")
+		return True
+	elif operacao == "-":
+		print ("menos")
+		return True
+	elif operacao == "*":
+		print("multiplicacao")
+		return True
+	elif operacao == "/":
+		print("divisao")
+		return True
+	else:
+		return False 
 
 primeironumero = input ("digite o primeiro número: ")
-
 print("digitado:",primeironumero)
-operacao = input ("digite a operação: ")
 
+operacao = input ("digite a operação: ")
 print("operacao digitada",operacao)
+
 segundonumero = input ("digite o segundo número:")
 print ("digitado",segundonumero)
+
+operacaoValida = operacaoValida(operacao)
+print(operacaoValida)
 
 if operacao == "+":	
 	resultado = somar(primeironumero, segundonumero)
@@ -46,5 +63,3 @@ elif operacao == "*":
 elif operacao == "/":
 	resultado = divisao(primeironumero,segundonumero)
 	print("resultado:", resultado)
-
-
