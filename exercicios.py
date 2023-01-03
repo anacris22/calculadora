bd289dbce2013c8d80ed574af55e9a3b41d78ecd
Anacris Dias de França Oliveira <anacrisdfranca@gmail.com>
2023-01-03 16:15:32 -0300

    estudo de for

---
+++ exercicios.py
@@ -0,0 +1,16 @@
+print ("exercicos")
+
+qualdatanatal = input("digite data de natal:")
+print("digitado", qualdatanatal)
+
+datanatal = 25
+             
+if datanatal == qualdatanatal:
+	print("você acertou")
+else:
+	print("você errou")
+
+
+	nome = "Nico"
+sobrenome = "Steppat"
+print(nome + sobrenome)
