5f676dcc5df7cf85d7861014b25eb5cf2c154857
Anacris Dias de França Oliveira <anacrisdfranca@gmail.com>
2023-01-03 16:41:27 -0300

    alteracao

--- JogoDeAdivinhacao.py
+++ JogoDeAdivinhacao.py
@@ -4,7 +4,7 @@ numero_secreto = 43
 total_de_tentativas = 3
 
 
-for rodada in range (1, total_de_tentativas):
+for rodada in range (1, total_de_tentativas + 1):
 	print("Tentativa {} de {}".format(rodada,total_de_tentativas))
 	chute_str = input ("Digite o seu número: ")
 	print("voce digitou", chute_str)
